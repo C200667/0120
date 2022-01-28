196cd7e87710d58ed097d2825f678cf7b3b7b838
C200667 <84270332+C200667@users.noreply.github.com>
2022-01-28 11:22:11 +0900

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,13 @@
+﻿<?php
+require_once("config.php");
+function db_conn() {
+    try {
+        $dbh = new PDO(DSN,DB_USER,DB_PASS);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    } catch (PDOException $e){
+        echo $e->getMessage();   // エラーメッセージ
+        exit;
+    }
+    return $dbh;
+}
